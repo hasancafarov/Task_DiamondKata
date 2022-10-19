debd22c0649305c69ea8d49a8792313e26501154
99450 <99450@DESKTOP-I8G17J0>
2022-10-19 23:24:50 +0400

    Add project files.

---
+++ Task_DiamondKata/DiamondKata.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_DiamondKata
+{
+    public class DiamondKata
+    {
+        private const char firstChar = 'A';
+        private const char underScore = '_';
+        private const char newLine = '\n';
+
+        public static string Generate(char lastChar)
+        {
+            if (lastChar == firstChar) return "A";            
+
+            var left = lastChar - firstChar;
+            var diamondLength = left + 1;
+
+            var lines = new List<string>();
+            var spacing = 1;
+            IEnumerable<int> chars = Enumerable.Range(firstChar, lastChar + 1 - firstChar);
+
+            foreach (var character in chars)
+            {
+                lines.Add(CreateLine(left, character, spacing));
+                --left;
+                ++diamondLength;
+                spacing = diamondLength - left - 2;
+            }
+
+            //Generate All
+            var half1 = string.Join(newLine, lines);
+            lines.Reverse();
+            lines.RemoveAt(0);
+            var half2 = string.Join(newLine, string.Join(newLine, lines));
+            return half1 + newLine + half2;
+        }
+
+
+        private static string CreateLine(int left, int character, int spacing)
+        {
+            var letter = Convert.ToChar(character);
+
+            var builder = new StringBuilder();
+            builder.Append(underScore, left);
+            builder.Append(letter);
+
+            if (letter == firstChar) return builder.ToString();
+
+            builder.Append(underScore, spacing);
+            builder.Append(letter);
+
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ Task_DiamondKata/Program.cs
@@ -0,0 +1,10 @@
+﻿using Task_DiamondKata;
+
+if (Environment.GetCommandLineArgs().Length < 2) throw new ArgumentException("Input must be letter");
+
+var letter = Convert.ToChar(Environment.GetCommandLineArgs()[1].ToUpper());
+if (!char.IsLetter(letter)) throw new ArgumentException("Input is not a letter");
+
+Console.WriteLine(DiamondKata.Generate(Convert.ToChar(letter)));
+
+Console.ReadKey();
